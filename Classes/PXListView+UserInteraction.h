35da6293b8ac6244e49f57791b424217b1ec0bdb
Alex Rozanski <alex@rozanski.com>
2011-03-27 12:49:10 +0100

    Updated gitignore

---
+++ Classes/PXListView+UserInteraction.h
@@ -0,0 +1,14 @@
+//
+//  PXListView+UserInteraction.h
+//  PXListView
+//
+//  Created by Alex Rozanski on 27/03/2011.
+//  Copyright 2011 Alex Rozanski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface PXListView (PXListView_UserInteraction)
+
+@end
---
+++ Classes/PXListView+UserInteraction.m
@@ -0,0 +1,14 @@
+//
+//  PXListView+UserInteraction.m
+//  PXListView
+//
+//  Created by Alex Rozanski on 27/03/2011.
+//  Copyright 2011 Alex Rozanski. All rights reserved.
+//
+
+#import "PXListView+UserInteraction.h"
+
+
+@implementation PXListView (PXListView_UserInteraction)
+
+@end
